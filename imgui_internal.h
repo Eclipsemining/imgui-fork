83b8a7f0881b86e46a3b6bfca3ef5c970329b2e3
Brianna Townsend <btownsend@eclipsemining.com>
2024-08-23 15:56:55 -0700

    Fixed font scale multiple parent bug

--- imgui_internal.h
+++ imgui_internal.h
@@ -2576,7 +2576,17 @@ public:
 
     // We don't use g.FontSize because the window may be != g.CurrentWindow.
     ImRect      Rect() const            { return ImRect(Pos.x, Pos.y, Pos.x + Size.x, Pos.y + Size.y); }
-    float       CalcFontSize() const    { ImGuiContext& g = *GImGui; float scale = g.FontBaseSize * FontWindowScale * FontDpiScale; if (ParentWindow) scale *= ParentWindow->FontWindowScale; return scale; }
+    float       CalcFontSize() const    {
+        ImGuiContext& g = *GImGui;
+        float scale = g.FontBaseSize * FontWindowScale * FontDpiScale;
+
+        auto p = ParentWindow;
+        while(p){
+            scale *= p->FontWindowScale;
+            p = p->ParentWindow;
+        }
+        return scale;
+    }
     float       TitleBarHeight() const  { ImGuiContext& g = *GImGui; return (Flags & ImGuiWindowFlags_NoTitleBar) ? 0.0f : CalcFontSize() + g.Style.FramePadding.y * 2.0f; }
     ImRect      TitleBarRect() const    { return ImRect(Pos, ImVec2(Pos.x + SizeFull.x, Pos.y + TitleBarHeight())); }
     float       MenuBarHeight() const   { ImGuiContext& g = *GImGui; return (Flags & ImGuiWindowFlags_MenuBar) ? DC.MenuBarOffset.y + CalcFontSize() + g.Style.FramePadding.y * 2.0f : 0.0f; }
